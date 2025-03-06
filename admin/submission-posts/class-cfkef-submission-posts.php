1ba139e752aca56d3e2e11ef98743ada5f7dd2c9
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-03-06 15:33:46 +0530

    Render form post meta fields.
    1. Render form post meta fields.
    2. Added a styling for form post meta fields.

--- admin/submission-posts/class-cfkef-submission-posts.php
+++ admin/submission-posts/class-cfkef-submission-posts.php
@@ -27,12 +27,21 @@ class CFKEF_Submission_Posts {
     public function __construct() {
         add_action( 'init', [ $this, 'register_post_type' ] );
         add_action('add_meta_boxes', [ $this, 'add_submission_meta_boxes' ]);
+        add_action('admin_enqueue_scripts', [ $this, 'enqueue_admin_scripts' ]);
+    }
+
+    /**
+     * Enqueue admin scripts
+     */
+    public function enqueue_admin_scripts() {
+        wp_enqueue_style('cfkef-submission-posts', CFL_PLUGIN_URL . 'admin/submission-posts/class-cfkef-submission-post.css', [], CFL_VERSION);
     }
 
     /**
      * Add admin menu
      */
     public function register_post_type() {
+        
         $labels = array(
             'name'                  => esc_html_x( 'Submissions', 'Post Type General Name', 'cool-formkit' ),
             'singular_name'         => esc_html_x( 'Submission', 'Post Type Singular Name', 'cool-formkit' ),
@@ -99,7 +108,7 @@ class CFKEF_Submission_Posts {
         // var_dump(get_the_ID());
         // if (get_post_type() === 'cfkef-submission') {
             add_meta_box( 'cfkef-submission-meta-box', 'Submission Details', [ $this, 'render_submission_meta_box' ], 'cfkef-submission', 'normal', 'high' );
-            add_meta_box( 'cfkef-form-info-meta-box', 'Form Info', [ $this, 'render_form_info_meta_box' ], 'cfkef-submission', 'normal', 'high' );
+            add_meta_box( 'cfkef-form-info-meta-box', 'Form Info', [ $this, 'render_form_info_meta_box' ], 'cfkef-submission', 'side', 'high' );
         // }
     }
 
@@ -107,13 +116,60 @@ class CFKEF_Submission_Posts {
      * Render submission meta box
      */
     public function render_submission_meta_box() {
-        echo '<h1>Submission Details</h1>';
+        $form_data = get_post_meta(get_the_ID(), '_cfkef_form_data', true);
+        
+        $this->render_field_html("cfkef-form-data", $form_data);
     }
 
     /**
      * Render form info meta box
      */
     public function render_form_info_meta_box() {
-        echo '<h1>Form Info</h1>';
+        $meta = get_post_meta(get_the_ID(), '_cfkef_form_meta', true);
+
+          // Update the form entry id in post meta
+        $submission_number = get_post_meta(get_the_ID(), '_cfkef_form_entry_id', true);
+  
+        // Update the form name in post meta
+        $form_name = get_post_meta(get_the_ID(), '_cfkef_form_name', true);
+  
+        // Update the element id in post meta
+        $element_id = get_post_meta(get_the_ID(), '_cfkef_element_id', true);
+
+        $post_id= isset($meta['page_url']['value']) ? url_to_postid(isset($meta['page_url']['value'])) : '';
+
+        $data=[
+            'Page Title' => array('value' => isset($meta['page_title']['value']) ? $meta['page_title']['value'] : ''),
+            'Page Url' => array('value' => isset($meta['page_url']['value']) ? $meta['page_url']['value'] : ''),
+            // 'Page Url' => array('value' => $post_id),
+            'Entry No.' => array('value' => $submission_number),
+            'Form Name' => array('value' => $form_name),
+            'Element ID' => array('value' => $element_id),
+        ];
+
+        $this->render_field_html("cfkef-form-info", $data);
+    }
+
+    private function render_field_html($type, $data) {
+        echo '<div id="' . esc_attr($type) . '" class="cfkef-field-wrapper">';
+        echo '<table class="cfkef-data-table">';
+        echo '<tbody>';
+        
+        foreach ($data as $key => $value) {
+            if(empty($value['value'])) {
+                continue;
+            }
+            $label = $value['title'] ?? $key;
+            echo '<tr class="cfkef-data-table-key">';
+            echo '<td>' . esc_html($label) . '</td>';
+            echo '</tr>';
+            echo '<tr class="cfkef-data-table-value">';
+            echo '<td>' . esc_html($value['value']) . '</td>';
+            echo '</tr>';
+        }
+        
+        echo '</tbody>';
+        echo '</table>';
+        echo '</div>';
     }
 }
