6cf021e43942e7a62cfd8021488a3b1faccf3e44
ravi <ravicoolplugins@gmail.com>
2025-03-31 18:28:57 +0530

    solve syntax error in Cool_FormKit-forms-editor.min file
    solve syntax error in Cool_FormKit-forms-editor.min file

--- build/js/Cool_FormKit-forms-fe.asset.php
+++ build/js/Cool_FormKit-forms-fe.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array(), 'version' => '2377d0db398f15764fea');
-=======
-<?php return array('dependencies' => array(), 'version' => '3ad7598aad01db61af43');
->>>>>>> b5ffa067160f2557e7cd0c249972f9d51260c3cc
+<?php return array('dependencies' => array(), 'version' => '9415ce201a898c2c84db');
