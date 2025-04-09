468e5e598d4723ae10c0db31ae3f3f50fb964c00
ravi <ravicoolplugins@gmail.com>
2025-04-09 16:57:50 +0530

    update recaptcha setting

--- admin/recaptcha/recaptcha-settings.php
+++ admin/recaptcha/recaptcha-settings.php
@@ -21,33 +21,28 @@ class Recaptcha_settings{
             self::$instance = new self();
         }
         return self::$instance;
-    }       
+    }
 
     /**
      * Constructor
      */
 
-     public static function replace_with_star($text){
-            $first_three = substr($text, 0, 3); // Get first 3 characters
-            $last_three = substr($text, -3);   // Get last 3 characters
-            $masked_length = strlen($text) - 6; // Length of the masked portion
-            
-            if ($masked_length > 0) {
-                $masked_part = str_repeat('*', $masked_length); // Replace with '*'
-                return $first_three . $masked_part . $last_three;
-            }
-            
-            return $text; // Return as is if it's too short
-     }
-
      public function recaptcha_setting_html_output(CFKEF_Dashboard $dashboard) {
 
 
         if($dashboard->current_screen('cool-formkit', 'recaptcha-settings')){
+
+
+            $this->handle_form_submit();
+
             ?>
 
                 <div class="cfkef-settings-box">
-                    <h3><?php esc_html_e('Recaptcha v2', 'cool-formkit'); ?></h3>
+                    <h3><?php esc_html_e('reCAPTCHA', 'cool-formkit'); ?></h3>
+
+
+    <form method="post" action="" class="cool-formkit-form">
+
 
                     <table class="form-table cool-formkit-table">
                             
@@ -56,7 +51,7 @@ class Recaptcha_settings{
                                     <label for="site_key_v2" class="cool-formkit-label"><?php esc_html_e('Site Key', 'cool-formkit'); ?></label>
                                 </th>
                                 <td class="cool-formkit-table-td">
-                                    <input type="text" id="site_key_v2" class="regular-text cool-formkit-input" value="<?php echo static::replace_with_star( get_option('cfl_site_key_v2') ); ?>"/>
+                                    <input type="text" id="site_key_v2" name="site_key_v2" class="regular-text cool-formkit-input" value="<?php echo get_option('cfl_site_key_v2'); ?>"/>
                                 </td>
                             </tr>
                             <tr>
@@ -64,13 +59,13 @@ class Recaptcha_settings{
                                     <label for="secret_key_v2" class="cool-formkit-label"><?php esc_html_e('Secret Key', 'cool-formkit'); ?></label>
                                 </th>
                                 <td class="cool-formkit-table-td">
-                                    <input type="text" id="secret_key_v2" class="regular-text cool-formkit-input" value="<?php echo static::replace_with_star( get_option('cfl_secret_key_v2') ); ?>"/>
+                                    <input type="text" id="secret_key_v2" name="secret_key_v2" class="regular-text cool-formkit-input" value="<?php echo get_option('cfl_secret_key_v2'); ?>"/>
                                 </td>
                             </tr>
                             
                     </table>
 
-                    <h3 class="cool-formkit-description"><?php esc_html_e('Recaptcha v3', 'cool-formkit'); ?></h3>
+                    <h3 class="cool-formkit-description"><?php esc_html_e('reCAPTCHA V3', 'cool-formkit'); ?></h3>
 
                     <table class="form-table cool-formkit-table">
                             
@@ -79,7 +74,7 @@ class Recaptcha_settings{
                                     <label for="site_key_v3" class="cool-formkit-label"><?php esc_html_e('Site Key', 'cool-formkit'); ?></label>
                                 </th>
                                 <td class="cool-formkit-table-td">
-                                    <input type="text" id="site_key_v3" class="regular-text cool-formkit-input" value="<?php echo static::replace_with_star( get_option('cfl_site_key_v3') ); ?>"/>
+                                    <input type="text" id="site_key_v3" name="site_key_v3" class="regular-text cool-formkit-input" value="<?php echo get_option('cfl_site_key_v3'); ?>"/>
                                 </td>
                             </tr>
                             <tr>
@@ -87,16 +82,16 @@ class Recaptcha_settings{
                                     <label for="secret_key_v3" class="cool-formkit-label"><?php esc_html_e('Secret Key', 'cool-formkit'); ?></label>
                                 </th>
                                 <td class="cool-formkit-table-td">
-                                    <input type="text" id="secret_key_v3" class="regular-text cool-formkit-input" value="<?php echo static::replace_with_star( get_option('cfl_secret_key_v3') ); ?>"/>
+                                    <input type="text" id="secret_key_v3" name="secret_key_v3" class="regular-text cool-formkit-input" value="<?php echo get_option('cfl_secret_key_v3'); ?>"/>
                                 </td>
                             </tr>
 
                             <tr>
                                 <th scope="row" class="cool-formkit-table-th">
-                                    <label for="thresshold_v3" class="cool-formkit-label"><?php esc_html_e('Thresshold', 'cool-formkit'); ?></label>
+                                    <label for="threshold_v3" class="cool-formkit-label"><?php esc_html_e('Score Threshold', 'cool-formkit'); ?></label>
                                 </th>
                                 <td class="cool-formkit-table-td">
-                                    <input type="number" id="thresshold_v3" class="regular-text cool-formkit-input" value="<?php echo get_option('cfl_thresshold_v3')?>" step="0.1"/>
+                                    <input type="number" id="threshold_v3" name="threshold_v3" class="regular-text cool-formkit-input" value="<?php echo get_option('cfl_threshold_v3')?>" min="0" max="1"  step="0.1"/>
                                     <p class="description cool-formkit-description"><?php esc_html_e('Score threshold should be a value between 0 and 1, default: 0.5', 'cool-formkit'); ?></p>
                                 </td>
                             </tr>
@@ -104,30 +99,16 @@ class Recaptcha_settings{
                     </table>
 
                     <div>
-                        <button id="recaptcha-submit" type="button">Save Changes</button>
+                        <button id="recaptcha-submit" type="submit" name="save">Save Changes</button>
                     </div>
+
+                    </form>
+
                 </div>
 
 
             <?php
         }
-    }
-
-    public function enqueue_admin_scripts(){
-
-
-
-        if (isset($_GET['page']) && $_GET['page'] === 'cool-formkit' ) {
-
-    
-            
-            wp_localize_script('cfkef-admin-script', 'ajax_object', array(
-                'ajax_url' => admin_url('admin-ajax.php'),
-                'nonce'    => wp_create_nonce('cfl_recaptcha_ajax_nonce')
-            ));
-    
-        }
-
     }
 
 
@@ -140,11 +121,79 @@ class Recaptcha_settings{
 
         return $tabs;
     }
-    
-    public function recaptcha_ajax_function() {
+
+
+    public function handle_form_submit() {
 
         // Security check
-        check_ajax_referer('cfl_recaptcha_ajax_nonce', 'nonce');
+
+        if(isset($_POST['save'])){
+
+
+        $pattern = "/(<script|<\/script>|onerror=|onload=|eval\(|javascript:|SELECT |INSERT |DELETE |DROP |UPDATE |UNION )/i";
+
+        if(isset($_POST['site_key_v2'])){
+
+            if (preg_match($pattern, $_POST['site_key_v2'])) {
+    
+                echo '<div class="notice notice-error is-dismissible"><p>' . esc_html__('Invalid Input.', 'cool-formkit') . '</p></div>';
+                return;
+            }
+        }
+
+        if(isset($_POST['secret_key_v2'])){
+
+            if (preg_match($pattern, $_POST['secret_key_v2'])) {
+    
+                echo '<div class="notice notice-error is-dismissible"><p>' . esc_html__('Invalid Input.', 'cool-formkit') . '</p></div>';
+
+
+                return;
+            }
+        }
+
+        if(isset($_POST['site_key_v3'])){
+
+            if (preg_match($pattern, $_POST['site_key_v3'])) {
+    
+                echo '<div class="notice notice-error is-dismissible"><p>' . esc_html__('Invalid Input.', 'cool-formkit') . '</p></div>';
+
+                return;
+            }
+        }
+
+        if(isset($_POST['secret_key_v3'])){
+
+            if (preg_match($pattern, $_POST['secret_key_v3'])) {
+    
+                echo '<div class="notice notice-error is-dismissible"><p>' . esc_html__('Invalid Input.', 'cool-formkit') . '</p></div>';
+
+                return;
+            }
+        }
+
+        if(isset($_POST['threshold_v3'])){
+
+            if($_POST['threshold_v3'] == ""){
+                $_POST['threshold_v3'] = 0.5;
+            }
+
+            if (preg_match($pattern, $_POST['threshold_v3'])) {
+                echo '<div class="notice notice-error is-dismissible"><p>' . esc_html__('Invalid Input.', 'cool-formkit') . '</p></div>';
+
+                return;
+            }
+
+
+            if(!preg_match('/^\d*\.?\d+$/', $_POST['threshold_v3'])){
+                echo '<div class="notice notice-error is-dismissible"><p>' . esc_html__('Invalid Input.', 'cool-formkit') . '</p></div>';
+
+                return;
+            }
+        }
+
+
+
 
         $site_key_v2  = isset($_POST['site_key_v2']) ? sanitize_text_field($_POST['site_key_v2']) : '';
         $secret_key_v2 = isset($_POST['secret_key_v2']) ? sanitize_text_field($_POST['secret_key_v2']) : '';
@@ -152,13 +201,15 @@ class Recaptcha_settings{
         $site_key_v3  = isset($_POST['site_key_v3']) ? sanitize_text_field($_POST['site_key_v3']) : '';
         $secret_key_v3 = isset($_POST['secret_key_v3']) ? sanitize_text_field($_POST['secret_key_v3']) : '';
 
-        $thresshold_v3 = isset($_POST['thresshold_v3']) ? $_POST['thresshold_v3'] : '';
+        $threshold_v3 = isset($_POST['threshold_v3']) ?  sanitize_text_field($_POST['threshold_v3']) : '';
 
-        if($thresshold_v3 > 1){
-            $thresshold_v3 = 1;
-        }else if($thresshold_v3 < 0){
-            $thresshold_v3 = 0;
+
+        if($threshold_v3 > 1){
+            $threshold_v3 = 1;
+        }else if($threshold_v3 < 0){
+            $threshold_v3 = 0;
         }
+        
 
 
         update_option( "cfl_site_key_v2",  $site_key_v2);
@@ -170,21 +221,18 @@ class Recaptcha_settings{
 
         update_option( "cfl_secret_key_v3",  $secret_key_v3);
 
-        update_option( "cfl_thresshold_v3",  $thresshold_v3);
+        update_option( "cfl_threshold_v3",  $threshold_v3);
 
-        // Process request
-        $response = array('message' => 'Data Updated!!!');
-    
-        // Send response
-        wp_send_json_success($response);
+    echo '<div class="notice notice-success is-dismissible"><p>' . esc_html__('Settings saved.', 'cool-formkit') . '</p></div>';
+
+    }
+
     }
 
 
     public function __construct() {
 
-        add_action('admin_enqueue_scripts', [ $this, 'enqueue_admin_scripts' ]);
         add_action('cfkef_render_menu_pages', [ $this, 'recaptcha_setting_html_output' ]);
-        add_action('wp_ajax_cfl_recaptcha_ajax_action', [ $this, 'recaptcha_ajax_function' ]);
         add_filter('cfkef_dashboard_tabs', [ $this, 'add_dashboard_tab' ]);
        
     }
