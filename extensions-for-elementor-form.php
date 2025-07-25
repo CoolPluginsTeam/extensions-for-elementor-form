a09db61f55593e52ad8ded01f49048bac8124a3b
ravi <ravicoolplugins@gmail.com>
2025-07-25 10:16:54 +0530

    update version and readme

--- extensions-for-elementor-form.php
+++ extensions-for-elementor-form.php
@@ -7,13 +7,13 @@
  * Author: Cool Plugins
  * Author URI: https://coolplugins.net/
  * Text Domain: extensions-for-elementor-form
- * Version: 2.5.0
+ * Version: 2.5.1
  * Requires at least: 6.2
  * Requires PHP: 6.2
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Requires Plugins: elementor
- * Elementor tested up to: 3.30.2
+ * Elementor tested up to: 3.30.3
  * Elementor Pro tested up to: 3.30.0
  */
 
@@ -31,7 +31,7 @@ if (! defined('ABSPATH')) {
 	exit();
 }
 
-define('CFL_VERSION','2.5.0');
+define('CFL_VERSION','2.5.1');
 define('PHP_MINIMUM_VERSION','7.4');
 define('WP_MINIMUM_VERSION','5.5');
 define( 'CFL_PLUGIN_MAIN_FILE', __FILE__ );
