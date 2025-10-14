e8fb05a0e010df8698cb6f9bf9de29ea107e720c
ravi <ravicoolplugins@gmail.com>
2025-10-14 15:32:03 +0530

    enque notice script when notice display

--- admin/review-notice.php
+++ admin/review-notice.php
@@ -29,7 +29,7 @@ class Review_notice
 	public function __construct()
 	{
 
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_feedback_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_review_notice_scripts' ) );
 		add_action('elementor/element/cool-form/section_form_options/after_section_end', array($this, 'add_review_notice'), 10, 2);
 
 		add_action('elementor/editor/before_enqueue_styles', array($this, 'editor_assets'));
@@ -125,10 +125,37 @@ class Review_notice
 		return $html;
 	}
 
-	function enqueue_feedback_scripts() {
-			wp_enqueue_style( 'cfl-admin-review-notice-css', CFL_PLUGIN_URL . 'admin/feedback/css/cfl-admin-review-notice.css', null, CFL_VERSION );
+	function enqueue_review_notice_scripts() {
 
+
+		if (! current_user_can('update_plugins')) {
+			return;
+		}
+
+		// get installation dates and rated settings
+		$installation_date = get_option($this->installation_date_option);
+		$alreadyRated      = get_option($this->review_option) != false ? get_option($this->review_option) : 'no';
+
+		// check user already rated
+		if ($alreadyRated == 'yes') {
+			return;
+		}
+
+		// grab plugin installation date and compare it with current date
+		$display_date = date('Y-m-d h:i:s');
+		$install_date = new DateTime($installation_date);
+		$current_date = new DateTime($display_date);
+		$difference   = $install_date->diff($current_date);
+		$diff_days    = $difference->days;
+
+
+		// check if installation days is greator then week
+		if (isset($diff_days) && $diff_days >= 3) {
+			wp_enqueue_style( 'cfl-admin-review-notice-css', CFL_PLUGIN_URL . 'admin/feedback/css/cfl-admin-review-notice.css', null, CFL_VERSION );
+	
 			wp_enqueue_script( 'cfl-admin-review-notice-js', CFL_PLUGIN_URL . 'admin/feedback/js/cfl-admin-review-notice.js', array( 'jquery' ), CFL_VERSION );
+		}
+
 
 
 
